546c438793a3b4c88010b1c1b0bdcbede01fc818
Riley Sutton <riley.d.sutton@gmail.com>
2021-10-04 20:58:29 +1100

    add more indicators to prediction

--- dataset_generator.py
+++ dataset_generator.py
@@ -1,37 +1,59 @@
 import pandas
 import numpy
+from tqdm import tqdm
 from sklearn.preprocessing import MinMaxScaler
 
-def generate_relative_frame(prices):
-    ref_price = prices[0]
-    relative_prices = [0.0]
+def generate_ohlcv(data, index):
+    return numpy.array([
+        data.loc[index]['Open'],
+        data.loc[index]['High'],
+        data.loc[index]['Low'],
+        data.loc[index]['Close'],
+        data.loc[index]['Volume'],
+    ])
 
-    for price in prices[1:]:
-        if ref_price != 0:
-            relative_prices.append(price / ref_price  - 1.0)
-        else:
-            quit('encountered zero ref value')
+def generate_relative_frame(data, start_index, window_length):
+    ref_ohlcv = generate_ohlcv(data, start_index)
+    outs = [
+        numpy.array([
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            ref_ohlcv[-1]
+        ])
+    ]
 
-    relative_prices = numpy.array(relative_prices)
+    for index in range(start_index + 1, start_index + window_length + 1):
+        curr_ohlcv = generate_ohlcv(data, index)
 
-    return relative_prices
+        curr_ohlc = curr_ohlcv[:-1]
+        curr_volume = curr_ohlc[-1]
+
+        rel_ohlc = (ref_ohlcv[:-1] / curr_ohlc - 1.0)
+
+        outs.append(numpy.concatenate([rel_ohlc, numpy.array([curr_volume])]))
+
+    return numpy.array(outs)
 
 def generate_dataset(file, window_length=16):
-    data = numpy.flip(pandas.read_csv(file)['Close'].replace(numpy.nan, 0.0).to_numpy())
+    data = pandas.read_csv(file)[::-1]
     samples = []
     labels = []
 
-    for i in range(len(data) - window_length - 1):
-        frame = generate_relative_frame(data[i:i+window_length+1])
-        samples.append(frame[:window_length])
-        labels.append(1.0 if frame[-1] > 0 else 0.0)
+    for index in tqdm(range(len(data) - window_length - 1)):
+        frame = generate_relative_frame(data, index, window_length)
+        samples.append(frame[:-1])
+        labels.append(1.0 if frame[-1][-2] > 0 else 0.0) #Close price
+
+    old_samples = numpy.array(samples)
 
     samples_scaler = MinMaxScaler(feature_range=(-1.0, 1.0))
-    samples, labels = numpy.array(samples), numpy.array(labels).reshape(-1, 1)
+    samples, labels = numpy.array(samples).reshape(-1, 5), numpy.array(labels)
 
     samples_scaler.fit(samples)
 
-    return samples_scaler.transform(samples), labels
+    return samples_scaler.transform(samples).reshape(-1, window_length, 5), labels
 
     
 
--- main_nn.py
+++ main_nn.py
@@ -1,18 +1,24 @@
 import keras
+import numpy
 
 from dataset_generator import generate_dataset
 
 model = keras.Sequential([
-    keras.layers.Dense(128, activation='relu'),
-    keras.layers.Dense(64, activation='relu'),
-    keras.layers.Dense(32, activation='relu'),
+    keras.layers.TimeDistributed(keras.Sequential([
+        keras.layers.Dense(64, activation='relu'),
+        keras.layers.Dense(64, activation='relu'),
+    ])),
+    keras.layers.LSTM(1),
     keras.layers.Dense(1, activation='sigmoid'),
 ])
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 train_x, train_y = generate_dataset('data/gemini_BTCUSD_1hr.csv')
+
+print(train_x.shape)
+
 val_x, val_y = generate_dataset('data/gemini_ETHUSD_1hr.csv')
 
-model.fit(train_x, train_y, batch_size=16, epochs=1)
+model.fit(train_x, train_y, batch_size=16, epochs=2)
 model.evaluate(val_x, val_y)
